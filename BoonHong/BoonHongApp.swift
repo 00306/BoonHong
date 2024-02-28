96189eb9d1884c15040bb10a938710705d9ad87f
Eric <real.dassy@gmail.com>
2024-02-29 01:07:22 +0900

    Initial Commit

---
+++ BoonHong/BoonHongApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoonHongApp.swift
+//  BoonHong
+//
+//  Created by 송지혁 on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BoonHongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoonHong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BoonHong
+//
+//  Created by 송지혁 on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
